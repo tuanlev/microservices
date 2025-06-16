b199cb2c410602fa134a125e9cc2989ff529fc44
tuanle13-git <tuanlv1303@gmail.com>
2025-06-16 15:25:37 +0700

    add loginDTO

---
+++ app/auth-service/src/main/java/com/tuan/authservice/dtos/LoginDTO.java
@@ -0,0 +1,14 @@
+package com.tuan.authservice.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDTO {
+    private String username;
+    private String password;
+
+}
