e005fe8f6c5993805bad3eb40e789e541cccfeab
tuanle13-git <tuanlv1303@gmail.com>
2025-06-16 15:52:12 +0700

    add valitdate registration

--- app/auth-service/src/main/java/com/tuan/authservice/repositories/UserRepository.java
+++ app/auth-service/src/main/java/com/tuan/authservice/repositories/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 import com.tuan.authservice.models.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -12,4 +13,6 @@ import java.util.Optional;
 public interface  UserRepository extends JpaRepository<User, Long> {
    Optional<User> findUserByUsername(String username) ;
    Optional<User> findUserById(Long id);
+   @Query(value = "SELECT * FROM users u WHERE u.username =:username",nativeQuery=true)
+   Optional<User> checkRegistrationConditions(String username);
 }
